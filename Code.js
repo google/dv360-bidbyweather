7ca9e91aab03b3f7e8808c529f2492af260f0956
Larissa Barra <lbarra@google.com>
2020-07-14 14:25:41 +0100

    Including usage of OpenWeatherMap's weekly forecast endpoint and necessary adjustments to do so.
    Change-Id: I5ba1774a0f44ae6d75ab5c6bcb4df3327b728db1

--- Code.js
+++ Code.js
@@ -20,20 +20,26 @@ products and are not formally supported.
 
 var doc = SpreadsheetApp.getActive();
 var WEATHER_SHEET_NAME = 'Weather';
-var LOG_SHEET_NAME = 'Log';
+var CITIES_SHEET_NAME = 'Cities';
+
 var COL_ADVERTISER_ID = 0;
 var COL_LINEITEM_ID = 1;
 var COL_LOCATION_NAME = 2;
-var COL_WEATHER_CONDITION = 3;
-var COL_WEATHER_DESCRIPTION = 4;
-var COL_WEATHER_TEMPERATURE = 5;
-var COL_WEATHER_HUMIDITY = 6;
-var COL_TIMEOFDAY = 7;
-var COL_LINEITEM_NEW_STATUS = 8;
-var COL_LINEITEM_STATUS = 10;
-var COL_LOG = 11;
+var COL_DAYS_LOOKUP = 3;
+var COL_WEATHER_CONDITION = 4;
+var COL_WEATHER_DESCRIPTION = 5;
+var COL_WEATHER_TEMPERATURE = 6;
+var COL_WEATHER_HUMIDITY = 7;
+var COL_TIMEOFDAY = 8;
+var COL_LINEITEM_NEW_STATUS = 9;
+var COL_LINEITEM_STATUS = 11;
+var COL_LOG = 12;
+
+var COL_CITY_NAME = 0;
+var COL_CITY_LATITUDE = 1;
+var COL_CITY_LONGITUDE = 2;
 var weatherSheet = doc.getSheetByName(WEATHER_SHEET_NAME);
-var logSheet = doc.getSheetByName(LOG_SHEET_NAME);
+var citiesSheet = doc.getSheetByName(CITIES_SHEET_NAME);
 var userProperties = PropertiesService.getUserProperties();
 
 
@@ -59,7 +65,7 @@ function onOpen() {
  * @private
  */
 function init_() {
-  if (!weatherSheet || !logSheet) {
+  if (!weatherSheet || !citiesSheet) {
     // We need setup and format the spreadsheet
     initSpreadsheet_();
   }
@@ -72,7 +78,94 @@ function init_() {
  * @private
  */
 function initSpreadsheet_() {
-  // TODO
+  if (!citiesSheet) {
+    doc.insertSheet(CITIES_SHEET_NAME, 2);
+    citiesSheet = doc.getSheetByName(CITIES_SHEET_NAME);
+    citiesSheet.setTabColor('green');
+    citiesSheet.getRange(1, COL_CITY_NAME + 1)
+        .setValue('City name');
+    citiesSheet.getRange(1, COL_CITY_LATITUDE + 1)
+        .setValue('Latitude');
+    citiesSheet.getRange(1, COL_CITY_LONGITUDE + 1)
+        .setValue('Longitude');
+    citiesSheet.getRange(1, 1, 1, 3).setBackground('#EFEFEF');
+    citiesSheet.getRange(1, 1, 1, 3).setFontWeight('bold');
+    citiesSheet.setFrozenRows(1);
+  }
+
+  if (!weatherSheet) {
+    doc.insertSheet(WEATHER_SHEET_NAME, 1);
+    weatherSheet = doc.getSheetByName(WEATHER_SHEET_NAME);
+    weatherSheet.setTabColor('blue');
+    weatherSheet.getRange(1, COL_ADVERTISER_ID + 1)
+        .setValue('DV360 Advertiser ID');
+    weatherSheet.getRange(1, COL_LINEITEM_ID + 1)
+        .setValue('DV360\nLine Item ID');
+    weatherSheet.getRange(1, COL_LOCATION_NAME + 1)
+        .setValue('API Location name');
+    weatherSheet.getRange(1, COL_DAYS_LOOKUP + 1)
+        .setValue('Days from today');
+    weatherSheet.getRange(1, COL_WEATHER_CONDITION + 1)
+        .setValue('Weather condition');
+    weatherSheet.getRange(1, COL_WEATHER_DESCRIPTION + 1)
+        .setValue('Weather description');
+    weatherSheet.getRange(1, COL_WEATHER_TEMPERATURE + 1)
+        .setValue('Weather temperature');
+    weatherSheet.getRange(1, COL_WEATHER_HUMIDITY + 1)
+        .setValue('Humidity');
+    weatherSheet.getRange(1, COL_TIMEOFDAY + 1)
+        .setValue('Local hour of day');
+    weatherSheet.getRange(1, COL_LINEITEM_NEW_STATUS + 1)
+        .setValue('Line Item Status to apply');
+    weatherSheet.getRange(1, COL_LINEITEM_NEW_STATUS + 2)
+        .setValue('<- Human-readable criteria ' + 
+            '(for readability only, not used by the tool)');
+    weatherSheet.getRange(1, COL_LINEITEM_STATUS + 1)
+        .setValue('Line Item Status');
+    weatherSheet.getRange(1, COL_LOG + 1)
+        .setValue('Log (latest)');
+
+    weatherSheet.getRange(1, 1, 1, 13)
+        .setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP)
+        .setFontWeight('bold');
+    weatherSheet.getRange('K2:K')
+        .setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
+    weatherSheet.getRange('M2:M')
+        .setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
+
+    weatherSheet.getRange(1, 1, 1, 4).setBackground('#ADD6AD');
+    weatherSheet.getRange(1, 5, 1, 9).setBackground('#FFF2CC');
+    weatherSheet.getRange(1, 10, 1, 11).setBackground('#ADD6AD');
+    weatherSheet.getRange(1, 12, 1, 13).setBackground('#FFF2CC');
+    weatherSheet.getRange('E2:I').setBackground('#EFEFEF');
+    weatherSheet.getRange('L2:M').setBackground('#EFEFEF');
+
+    weatherSheet.setFrozenRows(1);
+    weatherSheet.setColumnWidths(1, 2, 90);
+    weatherSheet.setColumnWidth(3, 130);
+    weatherSheet.setColumnWidth(4, 75);
+    weatherSheet.setColumnWidth(5, 85);
+    weatherSheet.setColumnWidth(6, 120);
+    weatherSheet.setColumnWidth(7, 90);
+    weatherSheet.setColumnWidths(8, 2, 75);
+    weatherSheet.setColumnWidth(10, 130);
+    weatherSheet.setColumnWidths(11, 3, 250);
+    weatherSheet.setColumnWidth(12, 200);
+
+    var daysRule = SpreadsheetApp
+        .newDataValidation()
+        .requireValueInList(['0', '1', '2', '3', '4', '5', '6', '7'])
+        .build();
+    var locationsRule = SpreadsheetApp
+        .newDataValidation()
+        .requireValueInRange(citiesSheet.getRange('A2:A'))
+        .build();
+    weatherSheet.getRange('C2:C').setDataValidation(locationsRule);
+    weatherSheet.getRange('D2:D').setDataValidation(daysRule);
+
+    weatherSheet.deleteRows(11, 988);
+    weatherSheet.deleteColumns(14, 13);
+  }
 }
 
 /**
--- WeatherUpdater.js
+++ WeatherUpdater.js
@@ -26,6 +26,8 @@ var MAX_RETRIES = 10;
 var SLEEP_BETWEEN_LOCATIONS = 1000; // in milliseconds.
 var SLEEP_BETWEEN_RETRIES = 3000; // in milliseconds.
 var retries = 0;
+var locationsSearched = {};
+var locationsCoordinates = {};
 
 
 /**
@@ -35,6 +37,8 @@ var retries = 0;
  * @public
  */
 function updateWeatherData() {
+  getLocationsCoordinates_();
+  locationsSearched = {};
   var values = weatherSheet.getDataRange().getValues();
   for (row in values) {
     if (row == 0) {
@@ -46,51 +50,99 @@ function updateWeatherData() {
           .setValue('Weather update failed: location name missing!');
       continue;
     }
+
     retries = 0;
-    var apiData = getWeatherFromApi_(locationString);
+    var apiData = null;
+    apiData = locationsSearched[locationString] 
+        || getWeatherFromApi_(locationString);
+
     if (!apiData) {
       weatherSheet.getRange(+row + 1, COL_LOG + 1)
           .setValue('Weather update failed: error with API call, despite ' +
           MAX_RETRIES + ' retries');
       continue;
     }
-    var weather = apiData.weather[0].main;
-    var weatherDetails = apiData.weather[0].description;
-    var temp = apiData.main.temp;
-    var humidity = apiData.main.humidity;
-    var timestamp = apiData.dt;
-    var timezone = apiData.timezone;
-    var adjustedTs = timestamp + timezone;
-    var d = new Date(adjustedTs * 1000);
+
+    var days = parseInt(weatherSheet.getRange(+row + 1, COL_DAYS_LOOKUP + 1)
+        .getValue()
+        .toString());
+
+    var weather = apiData.daily[days].weather[0].main;
+    var weatherDetails = apiData.daily[days].weather[0].description;
+    var temp = apiData.daily[days].temp.day;
+    var humidity = apiData.daily[days].humidity;
+    var timestamp = apiData.daily[days].dt;
+    var d = new Date(timestamp * 1000);
     var localHour = d.getUTCHours();
     var formattedDate = getFormattedDate_();
-    weatherSheet.getRange(+row + 1, COL_WEATHER_CONDITION + 1).setValue(weather);
-    weatherSheet.getRange(+row + 1, COL_WEATHER_DESCRIPTION + 1).setValue(weatherDetails);
-    weatherSheet.getRange(+row + 1, COL_WEATHER_TEMPERATURE + 1).setValue(temp);
-    weatherSheet.getRange(+row + 1, COL_WEATHER_HUMIDITY + 1).setValue(humidity);
-    weatherSheet.getRange(+row + 1, COL_TIMEOFDAY + 1).setValue(localHour);
+
+    weatherSheet.getRange(+row + 1, COL_WEATHER_CONDITION + 1)
+        .setValue(weather);
+    weatherSheet.getRange(+row + 1, COL_WEATHER_DESCRIPTION + 1)
+        .setValue(weatherDetails);
+    weatherSheet.getRange(+row + 1, COL_WEATHER_TEMPERATURE + 1)
+        .setValue(temp);
+    weatherSheet.getRange(+row + 1, COL_WEATHER_HUMIDITY + 1)
+        .setValue(humidity);
+    weatherSheet.getRange(+row + 1, COL_TIMEOFDAY + 1)
+        .setValue(localHour);
     weatherSheet.getRange(+row + 1, COL_LOG + 1)
         .setValue('Weather condition updated at ' + formattedDate);
+
+    locationsSearched[locationString] = apiData;
     Utilities.sleep(SLEEP_BETWEEN_LOCATIONS);
   }
 }
 
 /**
- * Calls OpenWeatherMap API and retrieves the current weather data for the
+ * Builds map of location names and their geographical coordinates.
+ * @private
+ */
+function getLocationsCoordinates_() {
+  cities = citiesSheet.getDataRange().getValues();
+
+  for (row in cities) {
+    locationsCoordinates[
+      citiesSheet.getRange(+row + 1, COL_CITY_NAME + 1)
+      .getValue()
+      .toString()
+    ] = {
+      'latitude': citiesSheet.getRange(+row + 1, COL_CITY_LATITUDE + 1)
+          .getValue()
+          .toString(),
+      'longitude': citiesSheet.getRange(+row + 1, COL_CITY_LONGITUDE + 1)
+          .getValue()
+          .toString()
+    };
+  }
+}
+
+/**
+ * Calls OpenWeatherMap API and retrieves the weather data for the week for the
  * location specified as input.
  * @param {string} location Location to query the weather API for.
  * @return {?Object} The weather API data for the location (null if error).
  * @private
  */
 function getWeatherFromApi_(location) {
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q='
-      + location + '&appid=' + getUserConfiguration_('ApiKey') +
+  var locationData = locationsCoordinates[location];
+  if (!locationData) {
+    return null;
+  }
+
+  var latitude = locationData.latitude;
+  var longitude = locationData.longitude;
+
+  var url = 'http://api.openweathermap.org/data/2.5/onecall?lat=' +
+      latitude + '&lon=' + longitude + '&exclude=current,minutely,hourly' +
+      '&appid=' + getUserConfiguration_('ApiKey') +
       '&units=' + getUserConfiguration_('Unit');
   retries++;
   try {
     var response = UrlFetchApp.fetch(url);
     return JSON.parse(response.getContentText());
   } catch (e) {
+    Logger.log(e);
     if (retries < MAX_RETRIES) {
       Utilities.sleep(SLEEP_BETWEEN_RETRIES);
       return getWeatherFromApi_(location);
